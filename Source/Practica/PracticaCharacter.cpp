37f152f0e45fd9c1a53fb450830e5091800868eb
Dana Garcia <141261839+danagg12@users.noreply.github.com>
2025-09-20 16:40:06 -0300

    TP: Unidad 1
    Parte A — Comentado de la plantilla
    Abrí el TuNombreCharacter.h y TuNombreCharacter.cpp .
    1-Encima de cada línea de código, escribí un comentario en español que explique:
    ● Qué hace la línea y por qué existe.
    ● si tiene parámetros modificar y probar que hace..(no hace falta compilar desde visual si los parámetros lo tocamos en el cpp).
    hacerlo desde editor.
    Ejemplo:
    //Configura la velocidad de caminata por defecto del Character al iniciar
    GetCharacterMovement()->MaxWalkSpeed = 500.f;
    
    Parte B — Inputs en C++
    Implementa 3 acciones (binding en SetupPlayerInputComponent): (investigar que esto y como funciona)
    ejemplo:
    Action = mostrarmensaje (cuando se presiona el imput muestra un mensaje)

--- Source/Practica/PracticaCharacter.cpp
+++ Source/Practica/PracticaCharacter.cpp
@@ -15,33 +15,42 @@
 APracticaCharacter::APracticaCharacter()
 {
 	// Set size for collision capsule
+	// Configura el tamaño de la capsula de colision
 	GetCapsuleComponent()->InitCapsuleSize(42.f, 96.0f);
 		
 	// Don't rotate when the controller rotates. Let that just affect the camera.
+	// Indica que el personaje no debe rotar con el controlador, asi9 la camara rota independiente del personaje.
 	bUseControllerRotationPitch = false;
 	bUseControllerRotationYaw = false;
 	bUseControllerRotationRoll = false;
 
 	// Configure character movement
+	// Configura el movimiento del personaje
+	// Se orienta automaticamente hacia la direccion en la que se mueve.
 	GetCharacterMovement()->bOrientRotationToMovement = true;
+
+	// Define la velocidad a la que puede rotar el personaje para alinearse con el movimiento.
 	GetCharacterMovement()->RotationRate = FRotator(0.0f, 500.0f, 0.0f);
 
 	// Note: For faster iteration times these variables, and many more, can be tweaked in the Character Blueprint
 	// instead of recompiling to adjust them
-	GetCharacterMovement()->JumpZVelocity = 500.f;
-	GetCharacterMovement()->AirControl = 0.35f;
-	GetCharacterMovement()->MaxWalkSpeed = 500.f;
-	GetCharacterMovement()->MinAnalogWalkSpeed = 20.f;
-	GetCharacterMovement()->BrakingDecelerationWalking = 2000.f;
-	GetCharacterMovement()->BrakingDecelerationFalling = 1500.0f;
+	// Define parametros basicos de movimiento:
+	GetCharacterMovement()->JumpZVelocity = 500.f; // Fuerza del salto.
+	GetCharacterMovement()->AirControl = 0.35f; // Que tanto puede moverse en el aire
+	GetCharacterMovement()->MaxWalkSpeed = 500.f; // Velocidad maxima al caminar
+	GetCharacterMovement()->MinAnalogWalkSpeed = 20.f; //Velocidad minima detectable en joysticks analogicos.
+	GetCharacterMovement()->BrakingDecelerationWalking = 2000.f; // Que tan rapido se frena al dejar de moverse
+	GetCharacterMovement()->BrakingDecelerationFalling = 1500.0f; // Que tan rapido se desacelera en el aire.
 
 	// Create a camera boom (pulls in towards the player if there is a collision)
+	// Crea el brazo de camara (spring arm)
 	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
 	CameraBoom->SetupAttachment(RootComponent);
-	CameraBoom->TargetArmLength = 400.0f;
-	CameraBoom->bUsePawnControlRotation = true;
+	CameraBoom->TargetArmLength = 400.0f; // Distancia de la camara al personaje
+	CameraBoom->bUsePawnControlRotation = true; // La camara rota con el controlador
 
 	// Create a follow camera
+	// Crea la camara que sigue al personaje
 	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
 	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
 	FollowCamera->bUsePawnControlRotation = false;
@@ -53,21 +62,45 @@ APracticaCharacter::APracticaCharacter()
 void APracticaCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	// Set up action bindings
+	// Comprueba si el componente de input es del tipo mejorado (Enhanced Input)
 	if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent)) {
 		
+		//Aca se asignan las teclas
+
 		// Jumping
+		// Cuando se presiona el boton llama a la funcion saltar
 		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Started, this, &ACharacter::Jump);
+		// Cuando se suelta el boton llama a la funcion de detener salto
 		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Completed, this, &ACharacter::StopJumping);
 
 		// Moving
+		// Asigna accion de movimiento
 		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &APracticaCharacter::Move);
+		// Asigna accion de movimiento de camara usando el mouse
 		EnhancedInputComponent->BindAction(MouseLookAction, ETriggerEvent::Triggered, this, &APracticaCharacter::Look);
 
 		// Looking
+		// Asigna accion de movimiento de camara usando joystick
 		EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &APracticaCharacter::Look);
+
+		/*---------------------------
+				  PARTE B
+		-----------------------------*/
+
+		// Bind de la accion mostrar mensaje
+		EnhancedInputComponent->BindAction(ShowMessageAction, ETriggerEvent::Started, this, &APracticaCharacter::ShowMessage);
+		
+		// Bind del dash
+		EnhancedInputComponent->BindAction(DashAction, ETriggerEvent::Started, this, &APracticaCharacter::Dash);
+
+		// Bind del crouch
+		EnhancedInputComponent->BindAction(CrouchAction, ETriggerEvent::Started, this, &APracticaCharacter::DoCrouchStart);
+		EnhancedInputComponent->BindAction(CrouchAction, ETriggerEvent::Completed, this, &APracticaCharacter::DoCrouchEnd);
+
 	}
 	else
 	{
+		// Si no encuentra el sistema Enhanced Input muestra un error en la consola
 		UE_LOG(LogPractica, Error, TEXT("'%s' Failed to find an Enhanced Input component! This template is built to use the Enhanced Input system. If you intend to use the legacy system, then you will need to update this C++ file."), *GetNameSafe(this));
 	}
 }
@@ -75,18 +108,22 @@ void APracticaCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputC
 void APracticaCharacter::Move(const FInputActionValue& Value)
 {
 	// input is a Vector2D
+	// Convierte el input en un vector 2D
 	FVector2D MovementVector = Value.Get<FVector2D>();
 
 	// route the input
+	// Llama a DoMove con esos valores (el DoMove es una funcion personalizada donde realmente se calcula hacia que direccion moverse y se aplica con AddMovementInput)
 	DoMove(MovementVector.X, MovementVector.Y);
 }
 
 void APracticaCharacter::Look(const FInputActionValue& Value)
 {
 	// input is a Vector2D
+	// Convierte el input en un vector 2D (X=yaw, Y=pitch)
 	FVector2D LookAxisVector = Value.Get<FVector2D>();
 
 	// route the input
+	// Llama a DoLook con esos valores (e� DoLook es una funcion personalizada que realmente aplica esa rotacion usando AddControllerYawInput(Yaw) y AddControllerPitchInput(Pitch)
 	DoLook(LookAxisVector.X, LookAxisVector.Y);
 }
 
@@ -95,16 +132,22 @@ void APracticaCharacter::DoMove(float Right, float Forward)
 	if (GetController() != nullptr)
 	{
 		// find out which way is forward
+		// Obtiene la rotacion actual del controlador
 		const FRotator Rotation = GetController()->GetControlRotation();
+
+		// Obtiene solo la rotacion en el Yaw
 		const FRotator YawRotation(0, Rotation.Yaw, 0);
 
 		// get forward vector
+		// Calcula la direccion adelante en el mundo segun la camara
 		const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
 
 		// get right vector 
+		// Calcula la direccion "derecha" en el mundo
 		const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
 
 		// add movement 
+		// Aplica movimiento en esas direcciones, escalado por los valores de entrada
 		AddMovementInput(ForwardDirection, Forward);
 		AddMovementInput(RightDirection, Right);
 	}
@@ -115,7 +158,9 @@ void APracticaCharacter::DoLook(float Yaw, float Pitch)
 	if (GetController() != nullptr)
 	{
 		// add yaw and pitch input to controller
+		// Aplica la rotacion horizontal (Yaw) a la camara
 		AddControllerYawInput(Yaw);
+		// Aplica la rotacion vertical (Pitch) a la camara.
 		AddControllerPitchInput(Pitch);
 	}
 }
@@ -123,11 +168,46 @@ void APracticaCharacter::DoLook(float Yaw, float Pitch)
 void APracticaCharacter::DoJumpStart()
 {
 	// signal the character to jump
+	// Llama a la funcion de salto de ACharacter
 	Jump();
 }
 
 void APracticaCharacter::DoJumpEnd()
 {
 	// signal the character to stop jumping
+	// Llama a la funcion que detiene el salto de ACharacter
 	StopJumping();
+}
+
+/*---------------------------
+		  PARTE B
+-----------------------------*/
+
+// defini la funcion para mostar un mensaje en la pantalla
+void APracticaCharacter::ShowMessage()
+{
+	GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Yellow, TEXT("Boton de mensaje presionado"));
+}
+
+// defini la funcion para hacer un dash, este tambien mostrara un mesaje cada vez que se ejecute la accion
+void APracticaCharacter::Dash()
+{
+	FVector Forward = GetActorForwardVector();
+	LaunchCharacter(Forward * 2500.0f, true, true);
+
+	GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Blue, TEXT("Se activo el dash"));
+}
+
+// Activa el agachado
+void APracticaCharacter::DoCrouchStart()
+{
+	Crouch();
+	GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, TEXT("Se activo el crouch"));
+}
+
+// Desactiva el agachado
+void APracticaCharacter::DoCrouchEnd()
+{
+	UnCrouch();
+	GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Blue, TEXT("Se desactivo el crouch"));
 }
--- Source/Practica/PracticaCharacter.h
+++ Source/Practica/PracticaCharacter.h
@@ -19,35 +19,59 @@ DECLARE_LOG_CATEGORY_EXTERN(LogTemplateCharacter, Log, All);
  *  Implements a controllable orbiting camera
  */
 UCLASS(abstract)
-class APracticaCharacter : public ACharacter
+class APracticaCharacter : public ACharacter // Define la clase APracticaCharacter, que hereda de ACharacter (personaje con movimiento basico en UE).
 {
 	GENERATED_BODY()
 
 	/** Camera boom positioning the camera behind the character */
+	// Variable que define el brazo (SpringArm) de la camara.
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Components", meta = (AllowPrivateAccess = "true"))
 	USpringArmComponent* CameraBoom;
 
 	/** Follow camera */
+	// Variable que define la camara
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Components", meta = (AllowPrivateAccess = "true"))
 	UCameraComponent* FollowCamera;
 	
 protected:
 
 	/** Jump Input Action */
+	// Definicion del Input Action para el salto
 	UPROPERTY(EditAnywhere, Category="Input")
 	UInputAction* JumpAction;
 
 	/** Move Input Action */
+	// Definicion del Input Action para el movimiento
 	UPROPERTY(EditAnywhere, Category="Input")
 	UInputAction* MoveAction;
 
 	/** Look Input Action */
+	// Deficinion del Input Action que controla la rotacion de la camara
 	UPROPERTY(EditAnywhere, Category="Input")
 	UInputAction* LookAction;
 
 	/** Mouse Look Input Action */
+	// Deficinion del Input Action para girar la camara utilizando el movimiento del mouse.
 	UPROPERTY(EditAnywhere, Category="Input")
 	UInputAction* MouseLookAction;
+
+
+	/*---------------------------
+			  PARTE B
+	-----------------------------*/
+
+	// Declare el input Action que luego voy a asignar desde el editor
+	// Lo que va a hacer este action es que cada vez que pressione la tecla se va a mostar un mesaje
+	UPROPERTY(EditAnywhere, Category = "Input")
+	UInputAction* ShowMessageAction;
+
+	// Igual que arriba declare este input que ejecutara un dash
+	UPROPERTY(EditAnywhere, Category = "Input")
+	UInputAction* DashAction;
+
+	//declare el input que ejecura el agachado
+	UPROPERTY(EditAnywhere, Category = "Input")
+	UInputAction* CrouchAction;
 
 public:
 
@@ -57,40 +81,69 @@ public:
 protected:
 
 	/** Initialize input action bindings */
+	// Se encarga de vincular los Input Action del jugador con las funciones que las ejecutan (para decirle a Unreal que funcion de tu personaje se ejecuta cuando el jugador aprieta un boton).
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 protected:
 
 	/** Called for movement input */
+	// Funcion que se ejecuta cada vez que el jugador mueve el stick o usa WASD en el teclado.
 	void Move(const FInputActionValue& Value);
 
 	/** Called for looking input */
+	// funcion que se ejecuta cuando el jugador mueve la camara.
 	void Look(const FInputActionValue& Value);
 
 public:
 
 	/** Handles move inputs from either controls or UI interfaces */
+	// Funcion que permite mover al personaje
 	UFUNCTION(BlueprintCallable, Category="Input")
 	virtual void DoMove(float Right, float Forward);
 
 	/** Handles look inputs from either controls or UI interfaces */
+	// Funcion que permite rotar la camara
 	UFUNCTION(BlueprintCallable, Category="Input")
 	virtual void DoLook(float Yaw, float Pitch);
 
 	/** Handles jump pressed inputs from either controls or UI interfaces */
+	// Funcion que se llama cuando el jugador presiona el boton de salto.
 	UFUNCTION(BlueprintCallable, Category="Input")
 	virtual void DoJumpStart();
 
 	/** Handles jump pressed inputs from either controls or UI interfaces */
+	// Funcion que ejecuta el final de un salto, cuando se suelta el boton
 	UFUNCTION(BlueprintCallable, Category="Input")
 	virtual void DoJumpEnd();
+
+	/*---------------------------
+	          PARTE B
+	-----------------------------*/
+
+	// Declare la funcion que muestra un mensaje en pantalla para bindearla desde C++
+	UFUNCTION(BlueprintCallable, Category = "Input")
+	void ShowMessage();
+
+	// Igual que arriba declare la funcion que ejecutara un dash para bindearla en C++
+	UFUNCTION(BlueprintCallable, Category = "Input")
+	void Dash();
+
+	// Funcion que activa el crouch para bindearla en C++
+	UFUNCTION(BlueprintCallable, Category = "Input")
+	virtual void DoCrouchStart();
+
+	// Declare la funcion que desactiva el crouch para bindearla desde C++
+	UFUNCTION(BlueprintCallable, Category = "Input")
+	virtual void DoCrouchEnd();
 
 public:
 
 	/** Returns CameraBoom subobject **/
+	// Es una funcion llamada GetCameraBoom() que devuelve el puntero al componente CameraBoom (devuelve el puntero al componente)
 	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
 
 	/** Returns FollowCamera subobject **/
+	// Es una funcion llamada GetFollowCamera() que devuelve el puntero a la cámara principal (FollowCamera) (devuelve el puntero al componente)
 	FORCEINLINE class UCameraComponent* GetFollowCamera() const { return FollowCamera; }
 };
 
